cd1531cafaf15fc34a778391f5aa8723d30fb6e3
Rafael Briones <rbrionesdev@outlook.com>
2021-08-23 22:10:10 -0600

    read specific notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,34 @@
 class NotesController < ApplicationController
  def index
+  puts "Index called"
   notes = Note.all
   render component: "Notes", props: { notes: notes } #Send to my react component
  end
+
+ def show
+  note = Note.find(params[:id])
+  render component: "Note", props: { note: note } 
+ end
+
+ def new
+    render component: "NewNote"
+ end
+
+ def create
+  # this creates a user in memory
+  note = Note.new(note_params)
+  if note.save
+    # go back to index
+    redirect_to notes_path
+  else
+    # invalid input
+    # TODO handle invalid input here
+  end
+ end
+
+ private
+ def note_params
+  params.require(:note).permit(:title, :description)
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/notes', to:'notes#index'
-  get '/notes/new', to:'notes#new'
+  get "/notes", to:"notes#index"
+  get "/notes/new", to:"notes#new"
+
+  post "/notes", to:"notes#create"
+  get "/notes/:id", to:"notes#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
